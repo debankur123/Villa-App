2ff9d2d3f631e862f8b277e2edeb48c5bb9623eb
DEBANKUR BHATTACHARJEE <debankurbhattacharjee784@gmail.com>
2024-08-16 23:02:21 +0530

    Added some Indentations in Villa Model

--- VillaApp.Domains/Entities/Villa.cs
+++ VillaApp.Domains/Entities/Villa.cs
@@ -1,16 +1,14 @@
-using System;
-
 namespace VillaApp.Domains.Entities;
 
 public class Villa
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public string? Description { get; set; }
-    public double? Price { get; set; }
-    public int? Sqft { get; set; }
-    public int? Occupancy { get; set; }
-    public string? ImageUrl { get; set; }
+    public int Id                { get; set; }
+    public required string Name  { get; set; }
+    public string? Description   { get; set; }
+    public double? Price         { get; set; }
+    public int? Sqft             { get; set; }
+    public int? Occupancy        { get; set; }
+    public string?   ImageUrl    { get; set; }
     public DateTime? CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
 }
